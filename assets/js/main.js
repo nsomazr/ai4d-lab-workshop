c6a0414356db7d69a3d5a957c71c867dcd7805ec
nsomazr <zreuben815@gmail.com>
2022-07-06 17:19:28 +0300

    dates

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,7 @@ var spy = new Gumshoe('#navigation a', {
 
 /* ======= Countdown ========= */
 // set the date we're counting down to
-var target_date = new Date("Oct 10, 2022").getTime();
+var target_date = new Date("Aug 10, 2022").getTime();
  
 // variables for time units
 var days, hours, minutes, seconds;
